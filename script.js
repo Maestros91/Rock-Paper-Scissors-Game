db359c31c76e027d4082b0a26f7a430895c151d3
Ioannis Mastrotheodorou <giannhsmastr@gmail.com>
2026-04-17 18:47:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,49 @@ let playerScore = 0;
 let computerScore = 0;
 let input = ""; 
 
+
+function isCancelled() {
+    if (input === null || input === "" || !gameArray.includes(input.toLowerCase())) { 
+        if (input === null) {
+            input = prompt('You can not escape from me..' +  '\nChoose wisely "Rock" 🤟🏼, "Paper" 🧻 or "Scissors" ✂️!');
+        } else {
+            input = prompt('I am sure you know how to write three easy words..' + '\nIt is "Rock" 🤟🏼, "Paper" 🧻 or "Scissors" ✂️!');
+        }
+        return false; 
+    } else {
+        playerSelection = input.toLowerCase();
+        return true;
+    }
+}
+
+function playRound(computer, player) {
+
+    const comp = computer.toLowerCase();
+    const play = player.toLowerCase();
+
+    if (comp === play) {
+        alert("It's a tie! 🤝 We both chose " + computer + ". Let's replay this round.");
+        input = prompt("Quick! Choose again: Rock 🤟🏼, Paper 🧻, or Scissors ✂️");
+        while (isCancelled() === false) { }
+        let newComputer = gameArray[Math.floor(Math.random() * gameArray.length)];
+        return playRound(newComputer, playerSelection);
+    }
+    let roundMessage = "";
+    if (
+        (player === "paper" && computer === "rock") ||
+        (player === "scissors" && computer === "paper") ||
+        (player === "rock" && computer === "scissors")
+    ) {
+        playerScore++;
+        roundMessage = "✅ Damn!! You took the round.. " + player + " wins " + computer ;
+    } else {
+        computerScore++;
+        roundMessage = "❌ Oooh Yeah.. " + computer + " beats " + player + ". Got you!";
+    }
+    console.log(roundMessage);
+    alert(roundMessage + "\nScore: Player " + playerScore + " - Computer " + computerScore);
+}
+
 function game() {
     playerScore = 0;
     computerScore = 0;
@@ -28,47 +71,4 @@ function game() {
     console.log("Game finished. Type game() to play again from 0-0.");
 }
 
-function isCancelled() {
-    if (input === null || input === "" || !gameArray.includes(input.toLowerCase())) { 
-        if (input === null) {
-            input = prompt('You can not escape from me..' +  '\nChoose wisely "Rock" 🤟🏼, "Paper" 🧻 or "Scissors" ✂️!');
-        } else {
-            input = prompt('I am sure you know how to write three easy words..' + '\nIt is "Rock" 🤟🏼, "Paper" 🧻 or "Scissors" ✂️!');
-        }
-        return false; 
-    } else {
-        playerSelection = input.toLowerCase();
-        return true;
-    }
-}
-
-
-
-function playRound(computer, player) {
-    if (computer === player) {
-        alert("It's a tie! 🤝 We both chose " + computer + ". Let's replay this round.");
-        input = prompt("Quick! Choose again: Rock 🤟🏼, Paper 🧻, or Scissors ✂️");
-        while (isCancelled() === false) { }
-        let newComputer = gameArray[Math.floor(Math.random() * gameArray.length)];
-        return playRound(newComputer, playerSelection);
-    }
-    let roundMessage = "";
-    if (
-        (player === "paper" && computer === "rock") ||
-        (player === "scissors" && computer === "paper") ||
-        (player === "rock" && computer === "scissors")
-    ) {
-        playerScore++;
-        roundMessage = "✅ Damn!! You took the round.. " + player + " wins " + computer ;
-    } else {
-        computerScore++;
-        roundMessage = "❌ Oooh Yeah.. " + computer + " beats " + player + ". Got you!";
-    }
-    console.log(roundMessage);
-    alert(roundMessage + "\nScore: Player " + playerScore + " - Computer " + computerScore);
-}
-
 game();
-
-
-
